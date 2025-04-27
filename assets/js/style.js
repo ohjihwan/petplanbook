35c1ba359177a32e6e11bde4fa1d7c1ff8cfb55d
오지환 <ovitwisdom@gmail.com>
2025-04-28 03:11:12 +0900

    메인 디자인 변경

--- assets/js/style.js
+++ assets/js/style.js
@@ -8,3 +8,28 @@ $('.gnb').on('mouseenter', function(){
 }).on('mouseleave', function(){
     $('.header').removeClass('-open-depth2')
 })
+
+
+var swiper = new Swiper(".suggestion-place .overflow-area", {
+    slideClass: "place",
+    observer:true,
+    observerParents:true,
+    autoplay:{
+        delay: 3000
+    },
+    loopedSlides: 10,
+    speed:500,
+    wahchOverflow:true,
+    effect: "coverflow",
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+});
