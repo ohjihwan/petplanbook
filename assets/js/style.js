cacc547464874cb64e0e2e042ecc654e6c47701f
오지환 <ovitwisdom@gmail.com>
2025-04-30 04:13:03 +0900

    기능 추가 및 마크업 변경

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,40 +1,105 @@
 
-// HM010
+function gnbMouseenter(){
+    $('.gnb').on('mouseenter', function(){
+        $('.header').addClass('-open-depth2')
+    }).on('mouseleave', function(){
+        $('.header').removeClass('-open-depth2')
+    })
+}
 
-//  mouseenter  mouseleave
+function ratingChecked(){
+    $('.rating input').on('click', function(){
+        if( $(this).is(':checked') == true ) {
+            $(this).closest('.rating').siblings('.rating').find('input').prop('checked', false)
+        }
+    });
+}
 
-$('.gnb').on('mouseenter', function(){
-    $('.header').addClass('-open-depth2')
-}).on('mouseleave', function(){
-    $('.header').removeClass('-open-depth2')
-})
+function mainSuggestionSwiper(){
+    let mainSuggestionSwiper = new Swiper(".suggestion-place .overflow-area", {
+        slideClass: "place",
+        observer:true,
+        observerParents:true,
+        autoplay:{
+            delay: 3000
+        },
+        loopedSlides: 10,
+        speed:500,
+        wahchOverflow:true,
+        effect: "coverflow",
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+    });
+}
 
-$('.rating input').on('click', function(){
-    if( $(this).is(':checked') == true ) {
-        $(this).closest('.rating').siblings('.rating').find('input').prop('checked', false)
+function detailImgs(){
+    let detailImgs = new Swiper(".detail-imgs", {
+        effect: "cards",
+        grabCursor: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
+
+function locationModalOpen(){
+    $('body').addClass('lock')
+    $('.modal').show()
+    detailImgs()
+}
+
+function modalOpenId(modalId){
+    $('body').addClass('lock')
+    $('#'+modalId).show()
+    console.log(modalId)
+}
+
+function modalClose(){
+    $('body').removeClass('lock')
+    $('.modal').hide()
+}
+
+function followingPath(){
+    const followingPath = $('.following-path')
+    let swiper = new Swiper(".following-path", {
+        direction: "vertical",
+        slidesPerView: "auto",
+        freeMode: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+        mousewheel: true,
+        resistanceRatio: 0,
+        on: {
+            reachBeginning: function () {
+                followingPath.addClass('-start').removeClass('-end');
+            },
+            reachEnd: function () {
+                followingPath.addClass('-end').removeClass('-start');
+            }
+        }
+    });
+    if ( followingPath.height() <= followingPath.find('ul').height() ) {
+        followingPath.addClass('-start')
     }
-});
+    if ( followingPath.height() > followingPath.find('ul').height() ) {
+        followingPath.addClass('-start')
+        followingPath.addClass('-end')
+    }
+}
 
-var swiper = new Swiper(".suggestion-place .overflow-area", {
-    slideClass: "place",
-    observer:true,
-    observerParents:true,
-    autoplay:{
-        delay: 3000
-    },
-    loopedSlides: 10,
-    speed:500,
-    wahchOverflow:true,
-    effect: "coverflow",
-    loop: true,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-});
+
+gnbMouseenter() // GNB
+ratingChecked() // 좋아요&싫어요
+mainSuggestionSwiper() // 메인 모션 스와이프
+followingPath() // 동선스크롤 이벤트
