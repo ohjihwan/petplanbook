9b8ccf2e34ebdc542d7f6898033525f21566f609
오지환 <ovitwisdom@gmail.com>
2025-05-02 16:38:05 +0900

    소스 코드 정리

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -5,17 +5,17 @@
 let map;
 	
 window.onload = function () {
-  // SDK 수동 로드
-  kakao.maps.load(function () {
-	// 지도 생성
-	map = new kakao.maps.Map(document.getElementById("map"), {
-	  center: new kakao.maps.LatLng(33.2539, 126.5596), // 서귀포시 중심
-	  level: 9
-	});
+	// SDK 수동 로드
+	kakao.maps.load(function () {
+		// 지도 생성
+		map = new kakao.maps.Map(document.getElementById("map"), {
+			center: new kakao.maps.LatLng(33.2539, 126.5596), // 서귀포시 중심
+			level: 9
+		});
 
-	// 초기 데이터 로딩 (원한다면 여기에 정의)
-	if (typeof loadData === "function") {
-	  loadData();
-	}
-  });
+		// 초기 데이터 로딩 (원한다면 여기에 정의)
+		if (typeof loadData === "function") {
+			loadData();
+		}
+	});
 };
