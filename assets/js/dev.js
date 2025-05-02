b5e8b35af379e6b81b3007720d0b0a655054ce08
오지환 <ovitwisdom@gmail.com>
2025-05-02 18:01:09 +0900

    맵 API 초기 세팅 연결 및 라이팅 문구 수정

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -2,20 +2,23 @@
 /******************************
  * 맵 AIP
 ******************************/
-let map;
-	
-window.onload = function () {
-	// SDK 수동 로드
-	kakao.maps.load(function () {
-		// 지도 생성
-		map = new kakao.maps.Map(document.getElementById("map"), {
-			center: new kakao.maps.LatLng(33.2539, 126.5596), // 서귀포시 중심
-			level: 9
-		});
 
-		// 초기 데이터 로딩 (원한다면 여기에 정의)
-		if (typeof loadData === "function") {
-			loadData();
-		}
-	});
-};
+function mapSeting() {
+	let map;
+	window.onload = function () {
+		kakao.maps.load(function () {
+			map = new kakao.maps.Map(document.getElementById("map"), {
+				center: new kakao.maps.LatLng(33.2539, 126.5596), // 서귀포시 중심
+				level: 9
+			});
+			if (typeof loadData === "function") {
+				loadData();
+			}
+		});
+	};
+
+
+
+
+}
+// mapSeting() // 맵 초기 세팅
