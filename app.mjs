0d981f9691d9415009b56a17832fe6c5233d6cad
gkrud <gkrud823@naver.com>
2025-05-08 09:10:17 +0900

    라우터

--- app.mjs
+++ app.mjs
@@ -1,30 +1,38 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
-import placeRouter from "./router/places.mjs";
 import userRouter from "./router/user.mjs";
+import routeRouter from "./router/route.mjs";
+import postRouter from "./router/post.mjs";
 
 const app = express();
 
-app.use(cors({
-	origin:true,
-	credentials:true
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(session({
-	secret: "your-secure-secret-key",
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		maxAge: 1000 * 60 * 60,
-	}
-}));
+app.use(
+  session({
+    secret: "your-secure-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 app.use("/uploads", express.static("uploads"));
 app.use("/assets", express.static("assets"));
 app.use("/", express.static("public"));
 app.use("/api/user", userRouter);
-app.use(placeRouter);
+app.use("/uploads", express.static("uploads")); // 정적 폴더 설정
+app.use(postRouter);
+
+app.use(routeRouter);
 
 app.listen(8080, () => {
-	console.log("서버 실행 중: http://localhost:8080");
+  console.log("서버 실행 중: http://localhost:8080");
 });
--- data/db.mjs
+++ data/db.mjs
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "1234",
+  password: "liberty",
   database: "database",
   waitForConnections: true,
   connectionLimit: 10,
