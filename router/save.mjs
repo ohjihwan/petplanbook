e4ad4b08816cc678b1fafeb63e639a03dbea55a1
오지환 <ohjihwan@naver.com>
2025-05-11 20:51:47 +0900

    Squashed commit of the following:
    commit 7298945862525195688c4d194265039c0ac989e2
    Author: bglyk83 <bglyk83@gmail.com>
    Date:   Sun May 11 16:54:32 2025 +0900
    
        변경사항
    
    commit 6bd3189623c77ebc47ac74a1a7e0dad550ec6335
    Author: bglyk83 <bglyk83@gmail.com>
    Date:   Sun May 11 16:48:17 2025 +0900
    
        WIP: 임시 커밋
    
    commit 43b1f86926b92e4dcc46e939e7364fe80780d190
    Author: bglyk83 <bglyk83@gmail.com>
    Date:   Sun May 11 16:41:55 2025 +0900
    
        변경 사항
    
    commit 613b54dda467c7f6866841b921b992eb55bf4171
    Author: bglyk83 <bglyk83@gmail.com>
    Date:   Sun May 11 16:38:33 2025 +0900
    
        my010 & my021 수정
    
    commit cd36902c86a8f81d6128b0a07b0ea60004e60faa
    Author: bglyk83 <bglyk83@gmail.com>
    Date:   Sun May 11 16:34:08 2025 +0900
    
        Add .vscode to .gitignore
    
    commit b7f42de62fe4c3971ef6059f38b07920ceb68f23
    Author: bglyk83 <bglyk83@gmail.com>
    Date:   Sun May 11 16:33:00 2025 +0900
    
        Remove .vscode from version control
    
    commit db55eefcccc7e37fae635f9faa93fe709c2c0029
    Author: YunKi Lee <bglyk83@gmail.com>
    Date:   Sun May 11 16:16:47 2025 +0900
    
        my010 장소 리스트에 추가 및 my021지도 로딩

--- router/save.mjs
+++ router/save.mjs
@@ -3,6 +3,20 @@ import db from "../data/db.mjs";
 
 const router = express.Router();
 
+router.post("/save-place", async (req, res) => {
+	const { title, addr1, tel, category } = req.body;
+	try {
+		await db.query(
+			"INSERT INTO saved_places (title, addr1, tel, category) VALUES (?, ?, ?, ?)",
+			[title, addr1, tel, category]
+		);
+		res.json({ success: true, message: "저장 성공" });
+	} catch (err) {
+		console.error("DB 오류:", err);
+		res.status(500).json({ success: false, message: "저장 실패" });
+	}
+});
+
 router.post("/save-route", async (req, res) => {
 	const { title, addr1, tel, category } = req.body;
 	try {
@@ -26,6 +40,20 @@ router.get("/save-places", async (req, res) => {
 	} catch (err) {
 		console.error("DB 조회 오류:", err);
 		res.status(500).json({ success: false, message: "조회 실패" });
+	}
+});
+
+router.delete("/save-places/:id", async (req, res) => {
+	const placeId = req.params.id;
+	try {
+		await db.query(
+			"DELETE FROM saved_places WHERE id = ?",
+			[placeId]
+		);
+		res.json({ success: true, message: "삭제 성공" });
+	} catch (err) {
+		console.error("DB 삭제 오류:", err);
+		res.status(500).json({ success: false, message: "삭제 실패" });
 	}
 });
 
